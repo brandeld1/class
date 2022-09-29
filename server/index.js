2e1ddc240a51b3b7087db8d60028535aabaa134b
brandeld1 <99097364+brandeld1@users.noreply.github.com>
2022-09-29 15:22:32 -0400

    port added

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
+
 const hostname = '127.0.0.1';
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.statusCode = 200;
@@ -9,6 +10,6 @@ app.get('/', (req, res) => {
   res.end('Hello World asdfg');
 })
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`)
 })
